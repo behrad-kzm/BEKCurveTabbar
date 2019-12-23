74c0a8c534587df09d1fe7ff8afb4b738afd1949
Behrad Kazemi <kazemi.behrad@golrang.com>
2019-12-23 17:43:19 +0330

    bugfix

--- BEKCurveTabbar/Classes/BEKTabBarViewController.swift
+++ BEKCurveTabbar/Classes/BEKTabBarViewController.swift
@@ -21,8 +21,4 @@ public class BEKTabBarViewController: UITabBarController {
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
 }
